fd98724831cf38100d380d41edc3fe06a44e9beb
tpeterson99 <160163927+tpeterson99@users.noreply.github.com>
2024-04-09 10:42:25 -0700

    Add files via upload

--- allExercisesInSeparateFiles/sketch.js
+++ allExercisesInSeparateFiles/sketch.js
@@ -4,7 +4,7 @@
 
 let currentActivity = 0;
 let menuButton, game1Button, game2Button, game3Button, game4Button, pauseButton;
-
+let bubbleHighScore = 0;
 /***** 
   * If you want to load images or sounds into your application,
   * try using preload()
@@ -28,6 +28,13 @@ function switchToMM(){
   game3Button.show();
   game4Button.show();
   pauseButton.hide();
+
+  bubbleScore.hide();
+  for (let i = 0; i < bubbleInstances.length; i++) {
+    let instance = bubbleInstances[i];
+    bubbleInstances.splice(i, 1);
+    bubbleCount = 0;
+  }
 }
 
 function pauseSetup() {
@@ -39,31 +46,31 @@ function setup() {
   background('blue');
   menuButton = createButton('Home Page');
   menuButton.size(100, 40);
-  menuButton.position(1000, 875);
+  menuButton.position(50, 50);
   menuButton.mousePressed(switchToMM);
   menuButton.hide();
   
-  game1Button = createButton('Game 1');
+  game1Button = createButton('Hungry Shark');
   game1Button.size(100, 40);
-  game1Button.position(200, 875);
+  game1Button.position(windowWidth/2 - 400, windowHeight/2);
   game1Button.mousePressed(game1Setup);
   game1Button.show();
   
-  game2Button = createButton('Game 2');
+  game2Button = createButton('Apple Draw');
   game2Button.size(100, 40);
-  game2Button.position(400, 875);
+  game2Button.position(windowWidth/2 - 200, windowHeight/2);
   game2Button.mousePressed(game2Setup);
   game2Button.show();
   
-  game3Button = createButton('Game 3');
+  game3Button = createButton('Bubble Pop');
   game3Button.size(100, 40);
-  game3Button.position(600, 875);
+  game3Button.position(windowWidth/2, windowHeight/2);
   game3Button.mousePressed(game3Setup);
   game3Button.show();
   
-  game4Button = createButton('Game 4');
+  game4Button = createButton('Bouncy Ball');
   game4Button.size(100, 40);
-  game4Button.position(800, 875);
+  game4Button.position(windowWidth/2 + 200, windowHeight/2);
   game4Button.mousePressed(game4Setup);
   game4Button.show();
 
@@ -92,14 +99,19 @@ function draw() {
       game4Draw();
       break;
   }
+  
 }
 
 function mainMenu(){
   background('blue');
   
+  noStroke();
   fill('black');
-  textSize(35);
-  text('Click an activity', 300, 750);
+  textSize(50);
+  text('Toddler\'s Playland', windowWidth/2 - 200, 100);
+
+  textSize(20);
+  text('Highscore: ' + bubbleHighScore, windowWidth/2, windowHeight/2 + 75);
 }
 
 /*****
@@ -118,3 +130,4 @@ function mousePressed(){
       break;
   }
 }
+
