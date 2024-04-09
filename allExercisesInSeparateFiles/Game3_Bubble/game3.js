ce5bb436f72d089861a8edb3c15c2a4720024ddd
tpeterson99 <160163927+tpeterson99@users.noreply.github.com>
2024-04-09 10:41:42 -0700

    Add files via upload

--- allExercisesInSeparateFiles/Game3_Bubble/game3.js
+++ allExercisesInSeparateFiles/Game3_Bubble/game3.js
@@ -1,31 +1,31 @@
-let bubbleMin = 20;
+let bubbleMin = 1000;
 let bubbleCount = 0;
 let popCount = 0;
+let bubbleInstances = [];
+let bubbleObj;
+let bubbleScore;
 
 function game3Preload(){
-  
+  bubbleObj = loadImage("./Game3_Bubble/fullbubble.png");
 }
 
 function bubbleCreation(addbubbleCount) {
-  stroke('black');
-  const c = color(5, 45, 90);
-  fill(c);
+  bubbleObj.loadPixels();
   for (let i = 0; i < addbubbleCount; i++) {
-    circle(random(75, 325),random(75, 325), 10);
+    let x = random(100, windowWidth - 100);
+    let y = random(100, windowHeight - 200);
+    bubbleObj.resize(100, 100);
+    bubbleInstances.push({ image: bubbleObj, x: x, y: y});
+    image(bubbleObj, x, y);
   }
 }
 
 function game3Setup(){
-  background(220);
+  background(75, 54, 209);
   currentActivity = 3;
-
-  let initialBubbleCount = random(25, 50);
+  popCount = 0;
   
   stroke(0);
-  line(50, 50, 50, 350);
-  line(50, 350, 350, 350);
-  line(350, 350, 350, 50);
-  line(350, 50, 50, 50);
   
   // Hide the Activity 3 button, show home and pause button
   menuButton.show();
@@ -35,25 +35,51 @@ function game3Setup(){
   game4Button.hide();
   pauseButton.show();
   
+  bubbleScore = createP("High Score: " + bubbleHighScore + " Current Score: " + popCount);
+  bubbleScore.position(250, 20);
+  bubbleScore.style('font-size', '32px');
+  
 }
 
 function game3Draw(){
   
-  let addBubble = random(25, 50);
-  console.log(typeof popCount);
-
+  let addBubble = random(10, 20);
+ 
   if (bubbleCount < bubbleMin) {
-    translate(width / 2, height / 2);
-    translate(p5.Vector.fromAngle(millis() / 1000, 40));
     bubbleCreation(addBubble);
     bubbleCount = addBubble + bubbleCount;
+  }
+
+  if (mouseIsPressed) {
+    game3MouseClicked();
   }
 }
 
 function game3MouseClicked(){
-
-  if (c === 0) {
-    c = 255;
-    popCount++;
+  for (let i = 0; i < bubbleInstances.length; i++) {
+    let instance = bubbleInstances[i];
+    if (mouseX >= instance.x && mouseX <= instance.x + instance.image.width && mouseY >= instance.y && mouseY <= instance.y + instance.image.height) {
+        bubbleInstances.splice(i, 1);
+        redrawBubbles();
+        bubbleCount--;
+        popCount++;
+        bubbleScore.html("High Score: " + bubbleHighScore + " Current Score: " + popCount);
+        updateHighscore(popCount);
+        break;
+    }
   }
 }
+
+function redrawBubbles() {
+  background(75, 54, 209);
+  for (let i = 0; i < bubbleInstances.length; i++) {
+    let instance = bubbleInstances[i];
+    image(instance.image, instance.x, instance.y);
+  }
+}
+
+function updateHighscore(score) {
+  if (score > bubbleHighScore) {
+    bubbleHighScore = score;
+  }
+} 
