f9fc1da11b1037818f60915ae384b3e6a1dc1e47
maithili0 <160162790+maithili0@users.noreply.github.com>
2024-04-11 10:57:39 -0700

    Update game4.js

--- allExercisesInSeparateFiles/Game4_Bouncy/game4.js
+++ allExercisesInSeparateFiles/Game4_Bouncy/game4.js
@@ -4,14 +4,11 @@ let ballX, ballY, ballSpeedY, score, gameOver;
 
 function game4Preload(){
   backgroundPhoto = loadImage("Game4/background.png");
-  
 }
 
 function game4Setup(){
-
   currentActivity = 4;
   backgroundPhoto.resize(windowWidth, windowHeight);
-  image(backgroundPhoto, 0, 0);
 
   menuButton.show();
   game1Button.show();
@@ -23,38 +20,44 @@ function game4Setup(){
   let y = random(windowHeight);
   let w = random(10, 40);
   let h = random(10, 40);
-  disc = new disc(x, y, w, h);
-  
-  ballX = random(windowWidth); // Start ball at random X position
-  ballY = 0; // Start ball at top of the screen
-  ballSpeedY = 5; // Set initial vertical speed of the ball
-  
+  disc = new Disc(x, y, w, h);
+
+  ballX = random(windowWidth);
+  ballY = 0;
+  ballSpeedY = 5;
+
   discWidth = 100;
   discHeight = 10;
-  discX = windowWidth / 2 - discWidth / 2; // Start disc at the middle of the screen horizontally
-  discY = windowHeight - 30; // Start disc at the bottom of the screen
-  
-  score = 0; // Initialize score
-  gameOver = false; // Set game over flag to false
+  discX = windowWidth / 2 - discWidth / 2;
+  discY = windowHeight - 30;
 
+  score = 0;
+  gameOver = false;
 }
 
 function game4Draw(){
-  image(backgroundPhoto, 0, 0); 
+  image(backgroundPhoto, 0, 0);
+  
 }
 
+function game4MousePressed(){
+  // Check if the mouse is pressed within the boundaries of the disc
+  if (mouseX > disc.x && mouseX < disc.x + disc.w &&
+      mouseY > disc.y && mouseY < disc.y + disc.h) {
+    // Handle mouse interaction for the disc
+    // For example, you can change the color or behavior of the disc
+    disc.color = color(255, 0, 0); // Change disc color to red
+  }
+}
 
-
+// Class definition for Disc
 class Disc {
   constructor(x, y, w, h) {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
-    this.offsetX = 0;
-    this.offsetY = 0;
-    this.dragging = false;
-    this.rollover = false;
+    this.color = color(); 
   }
 
   show(px, py) {
@@ -64,26 +67,19 @@ class Disc {
     }
 
     stroke(255);
-    noFill();
+    fill(this.color); // Use disc color
     rect(this.x, this.y, this.w, this.h);
   }
 
   pressed(px, py) {
     if (px > this.x && px < this.x + this.w && py > this.y && py < this.y + this.h) {
-      print("clicked on rect");
       this.dragging = true;
       this.offsetX = this.x - px;
-      // print(this.offsetX);
       this.offsetY = this.y - py;
-      // print(this.offsetY);
     }
   }
 
   notPressed(px, py) {
-    	print("mouse was released");
-      this.dragging = false;
-      
-
+    this.dragging = false;
   }
-
 }
