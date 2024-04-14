6d5a3dca543039988d5a889f06e41125e99e2c57
maithili0 <160162790+maithili0@users.noreply.github.com>
2024-04-13 20:05:46 -0700

    Update game4.js

--- allExercisesInSeparateFiles/Game4_Bouncy/game4.js
+++ allExercisesInSeparateFiles/Game4_Bouncy/game4.js
@@ -1,97 +1,100 @@
 let disc, discWidth, discHeight, discX, discY;
-let backgroundPhoto;
-let ballX, ballY, ballSpeedY, score, gameOver;
+let bgImage;
+let ballX, ballY, ballSpeedX, ballSpeedY, score, gameOver;
 
-function game4Preload(){
-  backgroundPhoto = loadImage("Game4/background.png");
-
+function preload() {
+  bgImage = loadImage("background.png");
 }
 
-function game4Setup(){
-  background("white");
+function setup() {
+  createCanvas(windowWidth, windowHeight);
 
-  currentActivity = 4;
+  // Initialize variables
+  discWidth = 100;
+  discHeight = 20;
+  discX = windowWidth / 2 - discWidth / 2;
+  discY = windowHeight - discHeight - 10; // Place disc at the bottom with a margin
 
-  // Hide the Activity 4 button, show all the other buttons
-  backgroundPhoto.resize(windowWidth, windowHeight);
-  image(backgroundPhoto, 0, 0);
+  ballX = random(windowWidth);
+  ballY = 0;
+  ballSpeedX = 5;
+  ballSpeedY = 5;
 
-  menuButton.show();
-  game1Button.hide();
-  game2Button.hide();
-  game3Button.hide();
-  game4Button.hide();
-}
+  score = 0;
+  gameOver = false;
 
-function game4Draw(){
-  background("white");
+  // Initialize disc
   let x = random(windowWidth);
   let y = random(windowHeight);
   let w = random(10, 40);
   let h = random(10, 40);
-  disc = new disc(x, y, w, h);
-
-  fill("white");
-  rect(10, 10, 100, 25);
-  fill("black");
-  text("Home Page", 25, 25);
-  ballX = random(windowWidth); // Start ball at random X position
-  ballY = 0; // Start ball at top of the screen
-  ballSpeedY = 5; // Set initial vertical speed of the ball
-
-  fill('black');
-  text('Activity 4 goes here', 200, 200);
-  discWidth = 100;
-  discHeight = 10;
-  discX = windowWidth / 2 - discWidth / 2; // Start disc at the middle of the screen horizontally
-  discY = windowHeight - 30; // Start disc at the bottom of the screen
-
-  score = 0; // Initialize score
-  gameOver = false; // Set game over flag to false
-
+  disc = new Disc(x, y, w, h);
 }
-function game4Draw(){
-  image(backgroundPhoto, 0, 0); 
+
+function draw() {
+  background(bgImage);
+
+  // Draw ball
+  fill(255);
+  ellipse(ballX, ballY, 50, 50);
+
+  // Move ball
+  if (!gameOver) {
+    ballY += ballSpeedY;
+    ballX += ballSpeedX;
+
+    // Check for collision with top and bottom edges
+    if (ballY <= 0 || ballY >= windowHeight) {
+      ballSpeedY *= -1; // Reverse Y direction
+    }
+
+    // Check for collision with left and right edges
+    if (ballX <= 0 || ballX >= windowWidth) {
+      ballSpeedX *= -1; // Reverse X direction
+    }
+
+    // Check for collision with disc
+    if (ballY + 10 >= discY && ballX >= discX && ballX <= discX + discWidth) {
+      ballSpeedY *= -1; // Reverse Y direction
+    }
+  }
+
+  // Draw disc
+  disc.show();
 }
+
+function mousePressed() {
+  disc.pressed(mouseX, mouseY);
+}
+
 class Disc {
   constructor(x, y, w, h) {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
+    this.color = color(255, 255, 255);
+    this.dragging = false;
     this.offsetX = 0;
     this.offsetY = 0;
-    this.dragging = false;
-    this.rollover = false;
   }
 
-  show(px, py) {
+  show() {
     if (this.dragging) {
-      this.x = px + this.offsetX;
-      this.y = py + this.offsetY;
+      this.x = mouseX + this.offsetX;
+      this.y = mouseY + this.offsetY;
     }
 
     stroke(255);
-    noFill();
+    fill(this.color);
     rect(this.x, this.y, this.w, this.h);
   }
 
   pressed(px, py) {
     if (px > this.x && px < this.x + this.w && py > this.y && py < this.y + this.h) {
-      print("clicked on rect");
       this.dragging = true;
       this.offsetX = this.x - px;
-      // print(this.offsetX);
       this.offsetY = this.y - py;
-      // print(this.offsetY);
     }
   }
-
-  notPressed(px, py) {
-    	print("mouse was released");
-      this.dragging = false;
-
-
-  }
-
 }
